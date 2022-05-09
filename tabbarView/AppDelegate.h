303791c4e1d4c61555c9b781b94b705a9bd79a24
hyunwooklee <lhw751@naver.com>
2022-05-09 14:07:20 +0900

    Initial Commit

---
+++ tabbarView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  tabbarView
+//
+//  Created by beintech on 2022/05/09.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ tabbarView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  tabbarView
+//
+//  Created by beintech on 2022/05/09.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
